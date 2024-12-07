6887e2bbac0f216fffa857ffc345ebf3d1d80a4b
ikman05 <ikman050505@gmail.com>
2024-12-07 18:42:53 +0300

    zov

--- code_1.py
+++ code_1.py
@@ -9,6 +9,7 @@ def start_message(message):
   markup= types.InlineKeyboardMarkup()
   markup.add(types.InlineKeyboardButton('Пидорское', callback_data='var1' ))
   markup.add(types.InlineKeyboardButton('Фундаментальное', callback_data='var2'))
+  markup.add(types.InlineKeyboardButton('Общеобразовательное', callback_data='var3'))
   bot.send_message(message.chat.id, "Начнем же смертельный бой", reply_markup= markup)
 
 @bot.callback_query_handler(func=lambda callback: True)
@@ -25,6 +26,12 @@ def callback_message(callback):
     markup_2.add(types.InlineKeyboardButton('Алгебра', callback_data='algebra'))
     markup_2.add(types.InlineKeyboardButton('Анализ', callback_data='analisys'))
     bot.send_message(callback.message.chat.id, 'Сначала разберемся с разделом:', reply_markup= markup_2)
+  elif callback.data == 'var3':
+    markup_3= types.InlineKeyboardMarkup()
+    markup_3.add(types.InlineKeyboardButton('Z', callback_data='algebra'))
+    markup_3.add(types.InlineKeyboardButton('O', callback_data='analisys'))
+    markup_3.add(types.InlineKeyboardButton('V', callback_data='analisys'))
+    bot.send_message(callback.message.chat.id, 'Сначала разберемся с разделом:', reply_markup= markup_3)
   
 bot.infinity_polling()
 
