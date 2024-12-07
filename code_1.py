c5353424058b02e2ff82037a1211c77aef23d228
ikman05 <ikman050505@gmail.com>
2024-12-07 23:33:21 +0300

    parsing_mf_one_file

--- code_1.py
+++ code_1.py
@@ -1,6 +1,8 @@
 import telebot
 from telebot import types
-
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
 
 token='7843616127:AAGn8WMZFim-9XgMn-CwirVRdgzR4w7MFgY'
@@ -38,3 +40,67 @@ def callback_message(callback):
   
 bot.infinity_polling()
 
+
+
+
+# URL_TEMPLATE = "https://mccme.ru/ru/nmu/courses-of-nmu/osen-20242025/"
+# r = requests.get(URL_TEMPLATE)
+# soup = bs(r.text, "html.parser")
+# # cours_names = list(set(map(str, soup.find_all('dd'))))
+# raw = set(soup.find_all('dd'))
+#
+# c = 0
+# for name in raw:
+#     separ = (str(name.a)).find(">") +1
+#     c+=1
+#     # print(str(name.a)[separ:-4], name.a["href"], c)
+#     print(name)
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+#
+# # URL страницы для парсинга
+# url = 'https://old.mccme.ru/ium/s24/s24.html'
+# # Отправляем GET-запрос к странице
+# response = requests.get(url)
+#
+# # Проверяем, что запрос выполнен успешно
+# if response.status_code == 200:
+#     # Парсим содержимое страницы
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#
+#     # Находим все элементы, содержащие названия курсов и ссылки
+#     courses = []
+#     for item in soup.select('dd'):  # Измените селектор в зависимости от структуры страницы
+#         print(item)
+
+
+import fitz
+pdf_path = '+ course_book_2425.pdf'
+raw = []
+current = {}
+with fitz.open(pdf_path) as pdf:
+    for i in range(1, 6):
+        page = pdf[i]
+        text = page.get_text()
+        if text:
+            lines = list(text.split('\n'))
+            for _ in lines:
+                if len(_) > 5 and ("Course descriptions" not in _):
+                    raw.append(_)
+    counter = raw.index("Описания курсов на русском") + 2
+    while counter < len(raw):
+        cc = -1
+        if '(' in raw[counter]:
+            cc = raw[counter].index("(")
+        name = raw[counter][:cc]
+        if raw[counter][-1].isnumeric():
+            page = raw[counter][-3::]
+        else:
+            page = raw[counter+1][-3::]
+            counter+=1
+        counter +=1
+        current[name] = page
+
+df = pd.DataFrame(list(current.items()), columns=['Ключи', 'Значения'])
+df.to_excel('output.xlsx', index=False)
